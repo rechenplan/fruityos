151c22e7b53321c8453acfcaee800a1358151527
orby <orby@ctrl-c.club>
2023-06-25 18:01:48 -0400

    Adding simple syntax error detection to lexer tester

--- peel/by/src/testers/lexer_tester.c
+++ peel/by/src/testers/lexer_tester.c
@@ -5,13 +5,14 @@
 extern char* tokens[];
 extern int nextToken(char* buffer, int* read, char* outBuf);
 
-void printProgram(char* program) {
+int printProgram(char* program) {
 	char buffer[256];
 	int read = 0;
 	int symbol = 0;
 
 	while (*program) {
 		symbol = nextToken(program, &read, buffer);
+		if (symbol == -4) return -1;
 		program += read;
 		if (symbol < 0) {
 			printf("%s\n", buffer);
@@ -20,11 +21,13 @@ void printProgram(char* program) {
 			printf("%s\n", tokens[symbol]);
 		}
 	}
+	return 0;
 }
 
 int main(int argc, char** argv) {
 	if (argc == 2) {
-		printProgram(argv[1]);
+		if (printProgram(argv[1]) < 0)
+			printf("SYNTAX ERROR\n");
 	} else {
 		printf("Usage: %s string\n", argv[0]);
 	}
