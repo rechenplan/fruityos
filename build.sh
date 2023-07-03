47c00dd4725765bfa4a9ddeb1768096207347ff6
orby <orby@ctrl-c.club>
2023-07-02 22:42:55 -0400

    Changing file extensions

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 mkdir bin
 
-../yuzu/bin/yuzu src/pish/pish.y
+../yuzu/bin/yuzu src/pish/pish.yuzu
 mv a.out bin/pish
 
-../yuzu/bin/yuzu src/cat/cat.y
+../yuzu/bin/yuzu src/cat/cat.yuzu
 mv a.out bin/cat
