865b750cf89e5632553ca5a2b6e3f2ebac549c5a
orby <orby@ctrl-c.club>
2023-07-03 13:24:10 -0400

    Changing cat to type, adding echo, supporting pish scripts

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ mkdir bin
 ../yuzu/bin/yuzu src/pish/pish.yuzu
 mv a.out bin/pish
 
-../yuzu/bin/yuzu src/cat/cat.yuzu
-mv a.out bin/cat
+../yuzu/bin/yuzu src/type/type.yuzu
+mv a.out bin/type
 
 ../yuzu/bin/yuzu src/dir/dir.yuzu
 mv a.out bin/dir
@@ -25,3 +25,6 @@ mv a.out bin/rmdir
 
 ../yuzu/bin/yuzu src/copy/copy.yuzu
 mv a.out bin/copy
+
+../yuzu/bin/yuzu src/echo/echo.yuzu
+mv a.out bin/echo
