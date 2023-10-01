51ef449e944719180d2e1aff645c66857cbbf215
rechenplan <>
2023-10-01 19:24:54 +0000

    Updating build scripts and yuzu script to use yc

--- src/yc/build.sh
+++ src/yc/build.sh
@@ -1,2 +1,8 @@
-../../bin/yuzu *.yuzu
+cat ../../lib/_start.asm > _.asm
+cat *.yuzu > _.yuzu
+../../bin/byc _.yuzu __.asm
+cat __.asm >> _.asm
+cat ../../lib/libpith.asm >> _.asm
+../../bin/zest _.asm a.out
 mv ./a.out ../../bin/yc
+rm _.asm _.yuzu __.asm
