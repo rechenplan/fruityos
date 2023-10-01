9e5b18d55e65cf9f5ff098c16086a98a574b89fd
rechenplan <>
2023-10-01 18:29:34 +0000

    Now compiling with yc instead of bootstrap compiler

--- start.sh
+++ start.sh
@@ -2,16 +2,16 @@
 
 mkdir bin tmp lib
 
-cp ../yuzu/bin/byc ./bin/byc
 cp ../yuzu/bin/zest ./bin/zest
+cp ../yuzu/bin/yc ./bin/yc
 cp ../yuzu/lib/libpith.asm ./lib/libpith.asm
 cp ../yuzu/lib/_start.asm ./lib/_start.asm
 
-bin/byc src/pish/pish.yuzu tmp/pish.asm
+bin/yc src/pish/pish.yuzu tmp/pish.asm
 cat lib/_start.asm tmp/pish.asm lib/libpith.asm > tmp/a.s
 bin/zest tmp/a.s bin/pish
 
-bin/byc src/concat/concat.yuzu tmp/concat.asm
+bin/yc src/concat/concat.yuzu tmp/concat.asm
 cat lib/_start.asm tmp/concat.asm lib/libpith.asm > tmp/a.s
 bin/zest tmp/a.s bin/concat
 
