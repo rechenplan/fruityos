58542ea922dae510b5d3d3a79688d38144acf5fe
orbitaldecay <orbitaldecay@protonmail.com>
2023-12-28 21:10:10 +0000

    Making value field in node larger (64 -> 256b)

--- src/zest/build.sh
+++ src/zest/build.sh
@@ -1,5 +1,5 @@
 cat main.yuzu lex.yuzu parse.yuzu emit.yuzu elf.yuzu > _.yuzu
-../../bin/yc _.yuzu _.asm
+../../bin/byc _.yuzu _.asm
 cat ../../lib/_start.asm _.asm ../../lib/libpith.asm > zest.asm
 nasm zest.asm -felf64 -o zest.o
 ld -n -s zest.o -o ../../bin/zest
