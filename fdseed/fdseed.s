6dadeb25f30b69d4b0956b6b9d8287f510813a90
orbitaldecay <orbitaldecay@protonmail.com>
2023-12-31 05:43:04 +0000

    Now boots to long mode

--- fdseed/fdseed.s
+++ fdseed/fdseed.s
@@ -1,83 +1,113 @@
 	org 0x7c00
 	bits 16
 
-CODE_SEG equ gdt_code - gdt
-DATA_SEG equ gdt_data - gdt
-
+	; Canonicalize IP
+	jmp 0x0000:start
 start:
-	; Display loading message
+	; Clear the direction flag
+	cld
+
+	; Clear data segment
 	xor ax, ax
 	mov ds, ax
-	mov si, boot_m
-	call puts
+
+	; Reset disk
+reset: 	mov ah, 0
+	int 0x13
 
 	; Load OS from disk (CHS)
-	mov ax, 0x027f	; Read 127 sectors (64k - 512 byte boot sector)
+	mov ax, 0x023e	; Read 62 sectors (we can't cross a cylinder boundry)
 	mov cx, 2	; Cylinder 0, Sector 2
 	mov dh, 0	; Head 0 (note dl already contains id of boot disk)
 	mov bx, 0x7e0	; ES:BX is output buffer (0x7e00)
 	mov es, bx
 	xor bx, bx
 	int 0x13
+	jc reset
 
 	; Turn on A20 gate (fast A20)
-	in al, 0x92
+a20:	in al, 0x92
 	or al, 2
 	out 0x92, al
 
-	; Disable interrupts
+	; Disable cursor
+	mov dx, 0x3d4
+	mov al, 0x0a
+	out dx, al
+	inc dx
+	mov al, 0x20
+	out dx, al
+
+	; Set up long mode paging (identity map first 2 MB)
+	mov edi, 0x1000
+	mov cr3, edi
+	xor eax, eax
+	mov ecx, 4096
+	rep stosd
+	mov edi, cr3
+	mov dword [edi], 0x2003
+	add edi, 0x1000
+	mov dword [edi], 0x3003
+	add edi, 0x1000
+	mov dword [edi], 0x4003
+	add edi, 0x1000
+	mov ebx, 3
+	mov ecx, 512
+page:	mov dword [edi], ebx
+	add ebx, 0x1000
+	add edi, 8
+	loop page
+
+	; Enable PAE
+pae:	mov eax, cr4
+	or eax, 1 << 5
+	mov cr4, eax
+
+	; Switch to long mode
 	cli
-
-	; Switch to protected mode
-	lgdt [gdtr]
-	mov eax, cr0
-	or al, 1
+	mov ecx, 0xc0000080	; LM-bit
+	rdmsr
+	or eax, 1 << 8
+	wrmsr
+	mov eax, cr0		; Enable paging and protected mode
+	or eax, 1 << 31 | 1 << 0
 	mov cr0, eax
-	jmp CODE_SEG:unpack
+	lgdt [gdtr]
+	jmp CODE_SEG:lmode
 
-	bits 32
+	bits 64
 
-unpack: mov ax, DATA_SEG
+lmode:	mov ax, DATA_SEG
 	mov ds, ax
+	mov es, ax
 	mov ss, ax
-	mov al, '$'
-	mov [0xb8000], al
-	mov al, 7
-	mov [0xb8001], al
 
-	; Unpack kernel
+	mov rdi, 0xb8000
+	mov rsi, boot_m
+print:	lodsb
+	test al, al
+	jz spin
+	stosb
+	mov al, 7
+	stosb
+	jmp print
 
 spin:	jmp spin
 
-	; Switch to long mode
-
-	; Put string subroutine
-	bits 16
-
-puts:	mov ah, 0x0e
-	lodsb
-	cmp al, 0
-	je puts.x
-	int 0x10
-	jmp puts
-puts.x:	ret
-
-boot_m: db "Booting FruityOS...", 13, 10, 0
-
-gdtr:
-	dw gdt_end - gdt - 1
-	dd gdt
+boot_m: db "Booting FruityOS...", 0
 
 gdt:
-gdt_null:
-	dq 0
+gdtr: ; null segment also
+	dw gdt_end - gdt - 1
+	dd gdt
+	dw 0
 
 gdt_code:
 	dw 0xffff	; limit
 	dw 0x0000	; base
 	db 0		; base
 	db 0x9a		; access byte
-	db 11001111b	; flags / limit
+	db 10101111b	; flags / limit
 	db 0		; base
 
 gdt_data:
@@ -90,10 +120,9 @@ gdt_data:
 
 gdt_end:
 
+CODE_SEG equ gdt_code - gdt
+DATA_SEG equ gdt_data - gdt
+
 times 510 - ($ - $$) db 0
 
 	dw 0xAA55
-
-
-; Normally we won't do this. We'll just concatenate the kernel.
-times 65536 - 512 db 0x80
