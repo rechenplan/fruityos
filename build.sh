457f571d4deccf317f6fd0a92f5141e11443f5cb
orbitaldecay <orbitaldecay@protonmail.com>
2023-12-31 20:03:06 +0000

    Moving around build scripts. Adding loc counting.

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
-cd src
-./build.sh
-cd ..
+../yuzu/bin/yc src/kmain.yuzu kmain.asm
+cat src/entry.s kmain.asm > _.asm
+nasm -f bin _.asm -o bin/kernel.bin
+rm kmain.asm _.asm
+../peel/bin/juicer c bin/kernel.bin bin/kernel.zip
+cat ../seed/bin/fdseed.bin bin/kernel.zip > bin/kernel.img
--- src/build.sh
+++
@@ -1,6 +0,0 @@
-../../yuzu/bin/yc kmain.yuzu kmain.asm
-cat entry.s kmain.asm > kernel.asm
-nasm -f bin kernel.asm -o ../bin/kernel.bin
-rm kmain.asm kernel.asm
-../../peel/bin/juicer c ../bin/kernel.bin ../bin/kernel.zip
-cat ../../seed/bin/fdseed.bin ../bin/kernel.zip > ../bin/kernel.img
