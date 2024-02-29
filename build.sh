13f38f7747882d68425490af7904b6aae2a570b0
orby <orby@Ubuntu.myguest.virtualbox.org>
2024-02-29 16:27:02 -0500

    Working on bootloader.

--- build.sh
+++ build.sh
@@ -5,3 +5,5 @@ nasm -f bin _.asm -o bin/kernel.bin
 rm kmain.asm _.asm
 ../peel/bin/juicer c bin/kernel.bin bin/kernel.zip
 cat ../seed/bin/fdseed.bin bin/kernel.zip > bin/kernel.img
+truncate -s 1440K bin/kernel.img
+qemu-system-x86_64 -drive format=raw,file=bin/kernel.img,index=0,if=floppy
