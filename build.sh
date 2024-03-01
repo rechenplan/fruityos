626160187efdbc1246aecde4f5b30c598cb15a0c
orby <orby@Ubuntu.myguest.virtualbox.org>
2024-03-01 17:52:27 -0500

    Updating build to use fap versions of utilities

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 mkdir bin
-../yuzu/bin/yc src/kmain.yuzu kmain.asm
+../peel/bin/yc.fap src/kmain.yuzu kmain.asm
 cat src/entry.s kmain.asm > _.asm
 nasm -f bin _.asm -o bin/kernel.bin
 rm kmain.asm _.asm
-../peel/bin/juicer c bin/kernel.bin bin/kernel.zip
+../peel/bin/juicer.fap c bin/kernel.bin bin/kernel.zip
 cat ../seed/bin/fdseed.bin bin/kernel.zip ../peel/peel.jz > bin/kernel.img
 truncate -s 1440K bin/kernel.img
 gdb --command=debug.gdb
