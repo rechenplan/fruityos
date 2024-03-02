18aaaee176c05083bdb8fbb030f9fb6787e4b9d4
orby <orby@Ubuntu.myguest.virtualbox.org>
2024-03-02 13:49:50 -0500

    peel.jz -> peel.jar

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ cat src/entry.s kmain.asm > _.asm
 nasm -f bin _.asm -o bin/kernel.bin
 rm kmain.asm _.asm
 ../peel/bin/juicer.fap c bin/kernel.bin bin/kernel.zip
-cat ../seed/bin/fdseed.bin bin/kernel.zip ../peel/peel.jz > bin/kernel.img
+cat ../seed/bin/fdseed.bin bin/kernel.zip ../peel/peel.jar > bin/kernel.img
 truncate -s 1440K bin/kernel.img
 gdb --command=debug.gdb
