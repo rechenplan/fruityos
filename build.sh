1769abc647396e38a70f6ff97b352810387179a8
orby <orby@Ubuntu.myguest.virtualbox.org>
2024-03-03 14:04:26 -0500

    Updating build scripts

--- build.sh
+++ build.sh
@@ -1,9 +1,6 @@
 mkdir bin
 ../peel/bin/yc.fap src/kmain.yuzu kmain.asm
 cat src/entry.s kmain.asm > _.asm
-nasm -f bin _.asm -o bin/kernel.bin
+nasm -f bin _.asm -o bin/pulp.bin
+../peel/bin/juicer.fap c bin/pulp.bin bin/pulp.sys
 rm kmain.asm _.asm
-../peel/bin/juicer.fap c bin/kernel.bin bin/kernel.sys
-cat ../seed/bin/fdseed.bin bin/kernel.sys ../peel/peel.jar > bin/fruityos.img
-truncate -s 252K bin/fruityos.img
-gdb --command=debug.gdb
--- debug.gdb
+++
@@ -1,6 +0,0 @@
-target remote | qemu-system-x86_64 -S -gdb stdio -m 16 -boot c -fda bin/kernel.img
-b *0x7c00
-b *0x10000
-c
-layout asm
-
