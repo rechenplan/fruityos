333f3ebf79346c94af36da045a033a159e5fab4d
orby <orby@Ubuntu.myguest.virtualbox.org>
2024-03-03 13:53:21 -0500

    Truncating to 252k

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ mkdir bin
 cat src/entry.s kmain.asm > _.asm
 nasm -f bin _.asm -o bin/kernel.bin
 rm kmain.asm _.asm
-../peel/bin/juicer.fap c bin/kernel.bin bin/kernel.zip
-cat ../seed/bin/fdseed.bin bin/kernel.zip ../peel/peel.jar > bin/kernel.img
-truncate -s 1440K bin/kernel.img
+../peel/bin/juicer.fap c bin/kernel.bin bin/kernel.sys
+cat ../seed/bin/fdseed.bin bin/kernel.sys ../peel/peel.jar > bin/fruityos.img
+truncate -s 252K bin/fruityos.img
 gdb --command=debug.gdb
