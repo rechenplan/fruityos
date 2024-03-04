173001285303b225077f8f927b0c2b446c11f8b9
orby <orby@Ubuntu.myguest.virtualbox.org>
2024-03-04 16:05:21 -0500

    Working on keyboard input

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 mkdir bin
 ../peel/bin/yc.fap src/kmain.yuzu kmain.asm
-cat src/entry.s kmain.asm > _.asm
+../peel/bin/yc.fap src/sys.yuzu sys.asm
+cat src/entry.s kmain.asm sys.asm > _.asm
 nasm -f bin _.asm -o bin/pulp.bin
 ../peel/bin/juicer.fap c bin/pulp.bin bin/pulp.sys
-rm kmain.asm _.asm
+rm kmain.asm sys.asm _.asm
