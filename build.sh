0c472b3cc697d332ef9a66678bd23284be7e71cb
orby <orby@Ubuntu.myguest.virtualbox.org>
2024-03-05 17:26:46 -0500

    Adding initrd unpacker

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 mkdir bin
-cat src/kmain.yuzu src/sys.yuzu src/x86.yuzu > pulp.yuzu
+cat src/kmain.yuzu src/sys.yuzu src/x86.yuzu src/utils.yuzu > pulp.yuzu
 ../peel/bin/yc.fap pulp.yuzu pulp.asm
 cat src/entry.s pulp.asm > _.asm
 nasm -f bin _.asm -o bin/pulp.bin
