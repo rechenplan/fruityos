bb9a079ae68982aee12fd51ce375f91a6f3dafc4
orby <orby@Ubuntu.myguest.virtualbox.org>
2024-03-10 13:10:30 -0400

    moving to subdir

--- build.sh
+++
@@ -1,7 +0,0 @@
-rm -rf bin
-mkdir bin
-../yuzu/bin/yc src/fapld.yuzu fapld.s
-cat ../yuzu/lib/_start.elf fapld.s src/write_ostab.s ../yuzu/lib/libpith.elf > _.s
-../yuzu/bin/zest e _.s bin/fapld
-rm _.s fapld.s
-sudo ./register.sh
