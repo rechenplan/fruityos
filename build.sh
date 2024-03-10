031ea219ff5c23d886c5deae63dc2d6b8b8e2d16
orby <orby@Ubuntu.myguest.virtualbox.org>
2024-03-10 10:49:59 -0400

    Cleaning up build script

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+rm -rf bin
 mkdir bin
 cat src/kmain.yuzu src/sys.yuzu src/x86.yuzu src/utils.yuzu src/ramfs.yuzu src/fba.yuzu src/list.yuzu src/inode.yuzu src/heap.yuzu src/global.yuzu src/scr.yuzu src/kb.yuzu src/fd.yuzu > pulp.yuzu
 ../peel/bin/yc.fap pulp.yuzu pulp.asm
