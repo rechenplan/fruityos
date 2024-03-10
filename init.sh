a3441d1756a0c7e18420d360b95b15412ef745d4
orby <orby@Ubuntu.myguest.virtualbox.org>
2024-03-10 13:29:08 -0400

    Removing extra junk from init

--- init.sh
+++ init.sh
@@ -1,6 +1,1 @@
 sudo apt install nasm gcc gdb qemu-system-x86
-git clone git@github.com:fruityos/pulp.git
-git clone git@github.com:fruityos/yuzu.git
-git clone git@github.com:fruityos/peel.git
-git clone git@github.com:fruityos/fapld.git
-git clone git@github.com:fruityos/seed.git
