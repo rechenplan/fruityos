fb91fb10578633254d9ad34f3fcc4b56fbfb9a36
orby <orbitaldecay@protonmail.com>
2024-06-18 23:18:16 +0200

    Adding build scripts and source code for peel to initramfs

--- build.sh
+++ build.sh
@@ -22,17 +22,26 @@ cd initrd
 mkdir lib
 mkdir bin
 mkdir src
+mkdir src/bin
 cp ../peel/bin/*.fap bin
 cp ../peel/lib/libpith.fap lib/libpith.asm
 cp ../peel/lib/_start.fap lib/_start.asm
+cd ../peel/src
+../bin/jar.elf c peel.jar
+../bin/juicer.elf c peel.jar peel.jar.jz
+cd ../../initrd
+cp ../peel/build.psh src
+cp ../peel/src/peel.jar.jz src
 ../peel/bin/jar.elf c initrd.jar
 cd ..
+rm -rf peel/src/peel.jar
+rm -rf peel/src/peel.jar.jz
 
 cat seed/bin/fdseed.bin pulp/bin/pulp.sys initrd/initrd.jar > fruityos_floppy.img
 cat seed/bin/hdseed.bin pulp/bin/pulp.sys initrd/initrd.jar > fruityos_hdd.img
 
 stat --printf="FruityOS size is %s bytes.\n" fruityos_floppy.img
 truncate -s 1440K fruityos_floppy.img
-truncate -s 64K fruityos_hdd.img
+truncate -s 128K fruityos_hdd.img
 find -regex '.*\.\(yuzu\|asm\|c\)' -print | xargs wc -l > loc.txt
 
