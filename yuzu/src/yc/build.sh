481800a1903f6992de5d50a5b964aa18157ece2b
orby <orbitaldecay@protonmail.com>
2024-06-21 02:15:58 +0200

    Fixing some bugs in build chain and yc emitter

--- yuzu/src/yc/build.sh
+++ yuzu/src/yc/build.sh
@@ -1,7 +1,9 @@
 cat *.yuzu > _yc.yuzu
 ../../bin/byc _yc.yuzu _yc.asm
 cat ../../lib/_start.elf _yc.asm ../../lib/libpith.elf > elf.asm
-cat ../../lib/_start.fap _yc.asm ../../lib/libpith.fap > fap.asm
 ../../bin/zest e elf.asm ../../bin/yc
+
+../../bin/yc _yc.yuzu _yc.asm
+cat ../../lib/_start.fap _yc.asm ../../lib/libpith.fap > fap.asm
 ../../bin/zest f fap.asm ../../bin/yc.bin
 rm elf.asm fap.asm _yc.yuzu _yc.asm
