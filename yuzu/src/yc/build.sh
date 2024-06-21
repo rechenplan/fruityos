9e4d9d171a54ff8ed1758240010176884af02f30
orby <orbitaldecay@protonmail.com>
2024-06-21 03:35:57 +0200

    Removing temp file during build that was missed.

--- yuzu/src/yc/build.sh
+++ yuzu/src/yc/build.sh
@@ -3,4 +3,4 @@ cat *.yuzu > _yc.yuzu
 cat ../../lib/_start.asm _yc.asm ../../lib/libpith.asm > yc.asm
 nasm yc.asm -felf64 -o yc.o
 ld -s -n yc.o -o ../../bin/yc
-rm _yc.yuzu yc.o yc.asm
+rm _yc.yuzu yc.o yc.asm _yc.asm
